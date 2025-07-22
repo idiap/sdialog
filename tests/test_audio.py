a792bde81fc0284d6887a14aeac022c0bfc16b04
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2025-07-22 20:31:33 +0200

    😢

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,19 +1,2 @@
-########################
-# Test the room module
-########################
-import sdialog.audio  # noqa: F401
-from sdialog.audio import room  # noqa: F401
-# from sdialog.audio import audio_dialog
-# from sdialog.audio import audio_turn
-# from sdialog.audio import audio_events
-# from sdialog.audio import audio_events_enricher
-# from sdialog.audio import audio_pipeline
-# from sdialog.audio import tts_engine
-# from sdialog.audio import evaluation
-# from sdialog.audio import room_generator
-# from sdialog.audio import voice_database
-# from sdialog.audio import whisper_normalizer
-
-
-def test_room_import():
+def test_audio_import():
     pass
