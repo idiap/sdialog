idiap/sdialog

commit c0f96ebf087b6a329f05d04f9b07233e9a4b2f14

Update datasets.py

src/sdialog/datasets.py
@@ -72,12 +72,9 @@ class STAR:
         :rtype: Union[str, dict]
         """
         with open(os.path.join(STAR._path, f"tasks/{task_name}/responses.json")) as reader:
-            responses = json.load(reader)
-            responses = {
-                key: re.sub(r"{(.+?)(?::\w+?)?}", lambda m: m.group(1).upper(), value)
-                for key, value in responses.items()
-                if key != "out_of_scope"
-            }
+            responses = {key: re.sub(r"{(.+?)(?::\w+?)?}", lambda m: m.group(1).upper(), value)
+                         for key, value in responses.items()
+                         if key != "out_of_scope"}
             return responses if as_dict else json.dumps(responses, indent=2)
 
     @staticmethod

commit 1c2d81a81dd32cde8f19e0551483ebf110881ffb

Fix error introduced in commit c0f96eb

src/sdialog/datasets.py
@@ -77,6 +77,7 @@ class STAR:
         :rtype: Union[dict, str]
         """
         with open(os.path.join(STAR._path, f"tasks/{task_name}/responses.json")) as reader:
+            responses = json.load(reader)
             responses = {key: re.sub(r"{(.+?)(?::\w+?)?}", lambda m: m.group(1).upper(), value)
                          for key, value in responses.items()
                          if key != "out_of_scope"}