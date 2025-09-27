8f7995ef6cb31b7773024ffceaaba35af698346f
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2025-09-27 18:13:06 +0200

    Add more informations about the languages of the TTS models.

--- src/sdialog/audio/tts_engine.py
+++ src/sdialog/audio/tts_engine.py
@@ -26,15 +26,30 @@ class BaseTTS:
 class KokoroTTS(BaseTTS):
     """
     Kokoro is a TTS engine that uses the Kokoro pipeline.
+    Available languages :
+        American English (a), British English (b), Spanish (e), French (f), Hindi (h),
+        Italian (i), Japanese (j), Brazilian Portuguese (p), Mandarin Chinese (z)
+    In order to use mandarin chinese and japanese, you need to install the corresponding package:
+        pip install misaki[zh] or pip install misaki[ja]
+    More details on: https://github.com/hexgrad/kokoro
     """
 
-    def __init__(self):
+    def __init__(
+            self,
+            lang_code="a"):
         """
         Initializes the Kokoro model.
         """
         from kokoro import KPipeline
 
-        self.pipeline = KPipeline(lang_code='a')
+        self.available_languages = ["a", "b", "e", "f", "h", "i", "j", "p", "z"]
+
+        if lang_code not in self.available_languages:
+            raise ValueError(f"Invalid language code: {lang_code}")
+
+        self.lang_code = lang_code
+
+        self.pipeline = KPipeline(lang_code=self.lang_code)
 
     def generate(self, text: str, voice: str) -> np.ndarray:
         """
@@ -51,6 +66,8 @@ class KokoroTTS(BaseTTS):
 class IndexTTS(BaseTTS):
     """
     IndexTTS is a TTS engine that uses the IndexTTS model.
+    Available languages: Chinese and English (the language is detected automatically by the text instruction)
+    More details on: https://github.com/index-tts/index-tts
     """
 
     def __init__(
@@ -79,13 +96,37 @@ class IndexTTS(BaseTTS):
 class ChatterboxTTS(BaseTTS):
     """
     Chatterbox is a TTS engine that uses the Chatterbox model.
+    Available languages:
+        Arabic (ar), Danish (da), German (de), Greek (el), English (en), Spanish (es),
+        Finnish (fi), French (fr), Hebrew (he), Hindi (hi), Italian (it), Japanese (ja),
+        Korean (ko), Malay (ms), Dutch (nl), Norwegian (no), Polish (pl), Portuguese (pt),
+        Russian (ru), Swedish (sv), Swahili (sw), Turkish (tr), Chinese (zh)
+    More details on: https://github.com/resemble-ai/chatterbox
     """
 
-    def __init__(self, device="cuda" if torch.cuda.is_available() else "cpu"):
+    def __init__(
+            self,
+            device="cuda" if torch.cuda.is_available() else "cpu",
+            lang_code="en"):
         """
         Initializes the Chatterbox model.
         """
-        from chatterbox.tts import ChatterboxTTS as ChatterboxTTSModel
+
+        self.available_languages = [
+            "ar", "da", "de", "el", "en", "es", "fi", "fr",
+            "he", "hi", "it", "ja", "ko", "ms", "nl", "no",
+            "pl", "pt", "ru", "sv", "sw", "tr", "zh"
+        ]
+
+        if lang_code not in self.available_languages:
+            raise ValueError(f"Invalid language code: {lang_code}")
+
+        self.lang_code = lang_code
+
+        if self.lang_code == "en":
+            from chatterbox.tts import ChatterboxTTS as ChatterboxTTSModel
+        else:
+            from chatterbox.mtl_tts import ChatterboxMultilingualTTS as ChatterboxTTSModel
 
         self.pipeline = ChatterboxTTSModel.from_pretrained(device=device)
 
@@ -94,7 +135,10 @@ class ChatterboxTTS(BaseTTS):
         Generate audio from text using the Chatterbox model.
         """
 
-        wav = self.pipeline.generate(text, audio_prompt_path=voice)
+        if self.lang_code == "en":
+            wav = self.pipeline.generate(text, audio_prompt_path=voice)
+        else:
+            wav = self.pipeline.generate(text, audio_prompt_path=voice, language_id=self.lang_code)
 
         return (wav.cpu().numpy().squeeze(), -1)
 
@@ -102,17 +146,36 @@ class ChatterboxTTS(BaseTTS):
 class XttsTTS(BaseTTS):
     """
     XTTS is a TTS engine that uses the XTTSv2 model from Coqui-TTS.
+    Available languages of the XTTSv2 model (https://huggingface.co/coqui/XTTS-v2):
+        English (en), Spanish (es), French (fr), German (de), Italian (it), Portuguese (pt),
+        Polish (pl), Turkish (tr), Russian (ru), Dutch (nl), Czech (cs), Arabic (ar), Chinese (zh-cn),
+        Japanese (ja), Hungarian (hu), Korean (ko) Hindi (hi)
+    More details on: https://github.com/coqui-ai/TTS
     """
+
+    DEFAULT_MODEL_NAME_XTTSV2 = "tts_models/multilingual/multi-dataset/xtts_v2"
 
     def __init__(
             self,
-            model_name="tts_models/multilingual/multi-dataset/xtts_v2",
+            model_name=DEFAULT_MODEL_NAME_XTTSV2,
             device=torch.cuda.is_available(),
-            progress_bar=True):
+            progress_bar=True,
+            lang_code="en"):
         """
         Initializes the XTTS model.
         """
         from TTS.api import TTS as XTTSModel
+
+        self.available_languages = [
+            "en", "es", "fr", "de", "it", "pt",
+            "pl", "tr", "ru", "nl", "cs", "ar",
+            "zh-cn", "ja", "hu", "ko", "hi"
+        ]
+
+        self.lang_code = lang_code
+
+        if model_name == XttsTTS.DEFAULT_MODEL_NAME_XTTSV2 and self.lang_code not in self.available_languages:
+            raise ValueError(f"Invalid language code: {self.lang_code}")
 
         self.pipeline = XTTSModel(
             model_name=model_name,
@@ -125,6 +188,6 @@ class XttsTTS(BaseTTS):
         Generate audio from text using the XTTSv2 model.
         """
 
-        audio = self.pipeline.tts(text=text, speaker_wav=voice, language="en")
+        audio = self.pipeline.tts(text=text, speaker_wav=voice, language=self.lang_code)
 
         return (np.array(audio), -1)
