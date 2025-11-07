2dcc327ed1a5f4651e46d9f276d862822d6abf70
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2025-11-07 18:29:55 +0100

    - Add ASR task - Add multi-dialog task annotation - Update the tutorial

--- src/sdialog/annotators/__init__.py
+++ src/sdialog/annotators/__init__.py
@@ -6,6 +6,7 @@ This module contains the classes for the annotators and the function to apply a 
 # SPDX-FileContributor: Yanis Labrak <yanis.labrak@univ-avignon.fr>
 # SPDX-License-Identifier: MIT
 
+import os
 import logging
 from enum import Enum
 from sdialog import Dialog
@@ -155,7 +156,7 @@ def apply_annotators(dialog: Dialog, annotators: List[Tuple[Annotator, dict[str,
     """
     Apply a list of annotators to a dialog.
     Each annotator is applied in the order of the list.
-    The dialog is modified in place.
+    The dialog is modified in place and the output is returned as a dialog.
     If an annotator has requirements, they are checked before applying the annotator.
 
     :param dialog: The dialog to annotate.
@@ -166,7 +167,88 @@ def apply_annotators(dialog: Dialog, annotators: List[Tuple[Annotator, dict[str,
     :rtype: Dialog
     """
 
+    # Apply the annotators to the dialog.
     for annotator, args in annotators:
-        dialog = annotator.annotate(dialog, args=args)
+
+        # if args["skip_if_existing"] and os.path.exists(args["save_path"]):
+        #     logging.info(
+        #         f"[Annotator] Using existing data from {args["save_path"]}"
+        #     )
+        #     dialog.set_annotations(
+        #         annotator.get_task_name(),
+        #         {
+        #             "data": pd.read_csv(args["save_path"]),
+        #             "modality": annotator.get_modality()
+        #         }
+        #     )
+        #     continue
+
+        # Apply the annotator and save the data.
+        dialog = annotator.annotate(
+            dialog=dialog,
+            args={
+                "save_path": args["save_path"]
+            }
+        )
 
     return dialog
+
+
+def apply_annotators_to_dialogs(
+    dialogs: List[Dialog],
+    annotators: List[Tuple[Annotator, dict[str, Any]]]
+) -> List[Dialog]:
+    """
+    Apply a list of annotators to a list of dialogs.
+    Each annotator is applied in the order of the list.
+    The dialogs are modified in place and the output is returned as a list of dialogs.
+    If an annotator has requirements, they are checked before applying the annotator.
+
+    :param dialogs: The list of dialogs to annotate.
+    :type dialogs: List[Dialog]
+    :param annotators: The list of annotators to apply.
+    :type annotators: List[Tuple[Annotator, dict[str, Any]]]
+    :return: The annotated list of dialogs.
+    :rtype: List[Dialog]
+    """
+
+    output_dialogs: List[Dialog] = []
+
+    # For each given dialog
+    for dialog in dialogs:
+
+        logging.info(f"[Annotator] Applying annotators to dialog {dialog.id}")
+
+        # Apply the annotators to the dialog.
+        for annotator, args in annotators:
+
+            # Build the path to save the data.
+            _path = f"{args['output_dir']}/{annotator.get_task_name()}/{dialog.id}.csv"
+
+            # Create the directory if it doesn't exist.
+            os.makedirs(os.path.dirname(_path), exist_ok=True)
+
+            # if args["skip_if_existing"] and os.path.exists(_path):
+            #     logging.info(
+            #         f"[Annotator] Using existing data from {_path}"
+            #     )
+            #     dialog.set_annotations(
+            #         annotator.get_task_name(),
+            #         {
+            #             "data": pd.read_csv(_path),
+            #             "modality": annotator.get_modality()
+            #         }
+            #     )
+            #     continue
+
+            # Apply the annotator and save the data.
+            dialog = annotator.annotate(
+                dialog=dialog,
+                args={
+                    "save_path": _path
+                }
+            )
+
+        output_dialogs.append(dialog)
+
+    return output_dialogs
--- src/sdialog/annotators/audio.py
+++ src/sdialog/annotators/audio.py
@@ -6,6 +6,7 @@ This module contains the classes for the audio-specific annotators.
 # SPDX-FileContributor: Yanis Labrak <yanis.labrak@univ-avignon.fr>
 # SPDX-License-Identifier: MIT
 
+import os
 import logging
 from typing import Any
 from sdialog import Dialog
@@ -121,6 +122,185 @@ class SpokenQuestionAnsweringAnnotator(Annotator):
                 "answer": ann["answer"],
                 "audio_path": audio_path,
                 "voice": audio_voice
+            })
+
+        dialog.add_annotations(self._task_name, _annotations)
+
+        logging.info(
+            "[SpokenQuestionAnsweringAnnotator] "
+            f"Annotation done for {len(_annotations['data'])} questions"
+        )
+
+        self.save(data=_annotations["data"], args=args)
+
+        return dialog
+
+
+class AutomaticSpeechRecognitionAnnotator(Annotator):
+    """
+    Annotator to convert a audio to text task to a automatic speech recognition task.
+    This annotator requires the AudioToTextAnnotator to be applied before.
+    The modality of the task is audio-to-text.
+    This annotator is specific to the AudioDialog class.
+    """
+
+    def get_modality(self) -> list[TaskModality]:
+        """
+        Get the modality of the automatic speech recognition task.
+        :return: The modality of the spoken question answering task.
+        :rtype: list[TaskModality]
+        """
+        return [
+            TaskModality.AUDIO_TO_TEXT
+        ]
+
+    def get_task_name(self) -> str:
+        """
+        Get the name of the automatic speech recognition task.
+        :return: The name of the spoken question answering task.
+        :rtype: str
+        """
+        return "automatic_speech_recognition"
+
+    def get_requirements(self) -> list[Annotator]:
+        """
+        Get the requirements for the automatic speech recognition task.
+        :return: The requirements for the spoken question answering task.
+        :rtype: list[Annotator]
+        """
+        return []
+
+    def save(self, data: Any, args: dict[str, Any] = {}) -> None:
+        """
+        Save the data to a file.
+        :param data: The data to save.
+        :type data: Any
+        :param args: Additional arguments to pass to the saving method.
+        It includes the 'save_path' where to save the data.
+        :type args: dict[str, Any]
+        :return: None
+        :rtype: None
+        """
+        import pandas as pd
+
+        if args is None or "save_path" not in args or args["save_path"] is None:
+            logging.warning("[AutomaticSpeechRecognitionAnnotator] No 'save_path' provided, skipping saving")
+            return
+
+        if not data:
+            logging.info("[AutomaticSpeechRecognitionAnnotator] No annotations to save, skipping file creation.")
+            return
+
+        df = pd.DataFrame(data)
+        df["identifier"] = df.index
+
+        df = df[[
+            "identifier",
+            "dialog_id",
+            "turn_id",
+            "start_time",
+            "end_time",
+            "original_audio_path",
+            "transcription",
+            "segment_path",
+            "voice",
+            "gender",
+            "age",
+            "language"
+        ]]
+
+        df.to_csv(args["save_path"], index=False)
+
+        logging.info(
+            "[AutomaticSpeechRecognitionAnnotator] "
+            f"Data saved to {args['save_path']}"
+        )
+
+    def annotate(self, dialog: Dialog, args: dict[str, Any] = {}) -> Dialog:
+        """
+        Annotate a dialog for automatic speech recognition tasks.
+        The annotation is a list of questions and answers, with the audio path and the voice used.
+        :param dialog: The dialog to annotate.
+        :type dialog: Dialog
+        :param args: Additional arguments to pass to the annotate method.
+        This includes the 'save_path' where to save the data and can contain 'save_args'
+        additional arguments to pass to the save method.
+        :type args: dict[str, Any]
+        :return: The annotated dialog.
+        :rtype: Dialog
+        """
+        logging.info("[AutomaticSpeechRecognitionAnnotator] Annotating dialog for automatic speech recognition tasks")
+
+        import librosa
+        import soundfile as sf
+        from sdialog.audio.dialog import AudioDialog
+
+        if not isinstance(dialog, AudioDialog):
+            raise ValueError("Dialog must be an instance of AudioDialog")
+
+        if "output_dir" not in args or args["output_dir"] is None:
+            raise ValueError("'output_dir' is required to save the annotations")
+
+        if len(dialog.audio_step_3_filepaths) == 0:
+            raise ValueError(
+                "No audio step 3 filepaths found in the dialog, "
+                "please run the audio pipeline for steps up to 3 or "
+                "provide a 'room_audio_path' in the arguments"
+            )
+
+        dialog = self.check_requirements(dialog)
+
+        _annotations = {
+            "data": [],
+            "modality": self._modality
+        }
+
+        if args is not None and "room_audio_path" in args and args["room_audio_path"] is not None:
+            _step_3_audio_path = args["room_audio_path"]
+        else:
+            _step_3_audio_path = list(dialog.audio_step_3_filepaths.values())[0]["audio_path"]
+            logging.warning(
+                "[AutomaticSpeechRecognitionAnnotator] No 'room_audio_path' provided, "
+                "using the first room audio path (step 3) found in the dialog."
+            )
+
+        # Load the audio file of the accoustic simulation of the dialog.
+        _wav_file, _sampling_rate = librosa.load(_step_3_audio_path)
+
+        for _turn_id, turn in enumerate(dialog.turns):
+
+            # Compute the start and end time of the segment.
+            _start_time = turn.audio_start_time
+            _end_time = turn.audio_start_time + turn.audio_duration
+
+            # Get the original text used to generate the audio of the turn.
+            _original_transcription = turn.text
+
+            # Section of the audio file that contains the transcription
+            _segment_data = _wav_file[int(_start_time * _sampling_rate):int(_end_time * _sampling_rate)]
+
+            # Build the path to the segment audio file.
+            _segment_path = f"./{args['output_dir']}/{dialog.id}/utterance_{dialog.id}_{_turn_id}.wav"
+            os.makedirs(os.path.dirname(_segment_path), exist_ok=True)
+
+            # Save the segment audio file.
+            sf.write(_segment_path, _segment_data, _sampling_rate)
+
+            _persona = dialog.personas[turn.speaker]
+
+            # Add the annotation to the spoken question answering task
+            _annotations["data"].append({
+                "dialog_id": dialog.id,
+                "turn_id": _turn_id,
+                "start_time": _start_time,
+                "end_time": _end_time,
+                "original_audio_path": _step_3_audio_path,
+                "transcription": _original_transcription,
+                "segment_path": _segment_path,
+                "voice": _persona["voice"],
+                "gender": _persona["gender"],
+                "age": _persona["age"],
+                "language": _persona["language"],
             })
 
         dialog.add_annotations(self._task_name, _annotations)
